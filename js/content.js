936a570e5d2d086818298eb256abffe71fbdfc2f
tuj41026 <tuj41026@temple.edu>
2022-10-27 20:53:49 -0400

    isCourses()
    Implemented isCourses() function that identifies whether user is on a courses page or not

--- js/content.js
+++ js/content.js
@@ -1,4 +1,7 @@
 const domain = window.location.origin;
+const url = window.location.href;
+
+//this takes over other webpages as well, for instance popup shows on "https://www.w3schools.com/js/js_window_location.asp"
 
 isDomain();
 function isDomain(){
@@ -6,3 +9,11 @@ function isDomain(){
         alert("On Canvas!")
     }
 }
+
+isCourses();
+function isCourses(){
+    if ((domain.includes("canvas") || domain.includes("instructure")) && url.includes("courses")) {
+            alert("On Courses!")
+        }
+}
+
