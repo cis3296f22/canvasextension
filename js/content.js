6b16eb504889eeb9e555a62381877c957283b3ba
tweiss9 <tylerhweiss@gmail.com>
2022-10-31 15:22:55 -0400

    Created toggle switch

--- js/content.js
+++ js/content.js
@@ -4,16 +4,24 @@ const url = window.location.href;
 //this takes over other webpages as well, for instance popup shows on "https://www.w3schools.com/js/js_window_location.asp"
 
 isDomain();
+isCourses();
+
 function isDomain(){
     if (domain.includes("canvas") || domain.includes("instructure") || domain.includes("learn") || domain.includes("school")) {
         alert("On Canvas!")
     }
 }
 
-isCourses();
 function isCourses(){
     if ((domain.includes("canvas") || domain.includes("instructure")) && url.includes("courses")) {
             alert("On Courses!")
         }
 }
 
+function toggle(button) {
+    if (button.value == "OFF") {
+      button.value = "ON";
+    } else {
+      button.value = "OFF";
+    }
+  }
