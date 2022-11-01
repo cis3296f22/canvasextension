14db11def2d9c349cb64580a45a88b73a99c7e62
Austin Foster <austinrfoster2001@gmail.com>
2022-11-01 10:16:23 -0400

    Started implementation of dark mode, figured out how to change colors of some webpage elements. Visually, it's really messy as of now.

--- js/content.js
+++ js/content.js
@@ -17,3 +17,12 @@ function isCourses(){
         }
 }
 
+
+darkMode();
+function darkMode() {
+    document.body.style.filter = "invert(1) hue-rotate(180deg)";
+    let media = document.querySelectorAll("img, picture, video");
+    media.forEach((mediaItem) => {
+        mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
+    })
+}
