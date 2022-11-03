066aea07f93b6faec238e09827f37b367cd95415
Austin Foster <austinrfoster2001@gmail.com>
2022-11-02 23:11:38 -0400

    Fixed messy visual of dark mode, now affects all of the html properties which properly displays a dark mode now.

--- js/content.js
+++ js/content.js
@@ -19,7 +19,7 @@ function isCourses(){
 
 function darkMode() {
 
-    document.body.style.filter = "invert(1) hue-rotate(180deg)";
+    document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
     let media = document.querySelectorAll("img, picture, video");
     media.forEach((mediaItem) => {
         mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
