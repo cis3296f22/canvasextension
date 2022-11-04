d56bc95d6041d78fdf568adb8a2d43218d6cbf30
tweiss9 <tylerhweiss@gmail.com>
2022-11-03 20:00:38 -0400

    toggle switch for dark mode

--- js/content.js
+++ js/content.js
@@ -12,18 +12,19 @@ function isDomain(){
 isCourses();
 function isCourses(){
     if ((domain.includes("canvas") || domain.includes("instructure")) && url.includes("courses")) {
-            alert("On Courses!")
+            // alert("On Courses!")
         }
 }
 
 
-function darkMode() {
-
-    document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-    let media = document.querySelectorAll("img, picture, video");
-    media.forEach((mediaItem) => {
-        mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
-    })
-    
-
-}
+function toggle() {
+    var lightMode = document.getElementById("style1"),
+        darkMode = document.getElementById("style2");
+    if (lightMode.disabled === "disabled") {
+        lightMode.disabled = undefined;
+      darkMode.disabled = "disabled";
+    } else {
+        lightMode.disabled = "disabled";
+      darkMode.disabled = undefined;
+    }
+  }
--- js/popup.js
+++ js/popup.js
@@ -2,19 +2,34 @@
 
 //gets id of current tab so you can edit style
 var tabId;
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	var activeTab = tabs[0];
-	tabId = activeTab.id; // or do whatever you need
-
- });
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  var activeTab = tabs[0];
+  tabId = activeTab.id; // or do whatever you need
+});
 
 //functionality of dark mode button
-document.addEventListener('DOMContentLoaded', function() {
-	var checkButton = document.getElementById('darkModeButton');
-	checkButton.addEventListener('click', function() {
-	chrome.scripting.executeScript({
-		target: {tabId: tabId, allFrames: true},
-		files: ['js/darkMode.js'],
-	},);
-	}, false);
-  }, false);
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var darkModeToggle = document.getElementById("darkModeToggle");
+    darkModeToggle.addEventListener(
+      "click",
+      function () {
+        if (this.checked){
+          chrome.scripting.executeScript({
+            target: { tabId: tabId, allFrames: true },
+            files: ["js/darkMode.js"],
+          });
+		}
+		else{
+			chrome.scripting.executeScript({
+				target: { tabId: tabId, allFrames: true },
+				files: ["js/undoDarkMode.js"],
+			  });
+		}
+      },
+      false
+    );
+  },
+  false
+);
