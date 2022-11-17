9c605d67bd9c55694321eee6ab418fce9e67b13f
tuj41026 <tuj41026@temple.edu>
2022-11-17 08:43:41 -0500

    Color choice changes menu background

--- js/popup.js
+++ js/popup.js
@@ -22,6 +22,7 @@ function popup(tabs, storage, scripting, doc) {
     doc.getElementById("sideMenuButton6").addEventListener("click", rmHelpClick);
     doc.getElementById("sideMenuButton7").addEventListener("click", rmHistoryClick);
     doc.getElementById("sideMenuButton8").addEventListener("click", rmInboxClick);
+    doc.getElementById("colorPicker").addEventListener("click", colorChoice);
 
     doc
       .getElementById("darkModeToggle")
@@ -150,10 +151,10 @@ function popup(tabs, storage, scripting, doc) {
     });
   }
 
-  // function colorChoice() {
-  //   var x = document.getElementById("colorpicker").value;
-  //   document.body.style.backgroundColor = x;
-  // }
+  function colorChoice() {
+     let color = document.getElementById('chooser').value;
+     document.body.style.backgroundColor = color;
+  }
 
   return {
     onDarkModeToggle: onDarkModeToggle,
