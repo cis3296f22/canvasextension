0ced26fb1a1229afb862b116f6ab3a41c27ff167
tweiss9 <tylerhweiss@gmail.com>
2022-11-17 18:20:21 -0500

    end of testing

--- content.test.js
+++ content.test.js
@@ -1,36 +1,51 @@
 const content = require("./js/content.js");
 
-var mockTabs;
 var mockDocument;
-var mockScripting;
 var mockStorage;
+var mockWindow;
 
 beforeEach(() => {
-  mockTabs = {
-    query: (q, cb) => {
-      cb([{ id: 100 }]);
-    },
-  };
 
   mockDocument = {
-    addEventListener: jest.fn(),
-    getElementById: jest.fn(),
-  };
-
-  mockScripting = {
-    executeScript: jest.fn(),
+    body: {
+      style: {},
+    },
+    querySelector: jest.fn(),
   };
 
   mockStorage = {
     get: jest.fn(),
     set: jest.fn(),
   };
+
+  mockWindow = {
+    location: {origin: "https://templeu.instructure.com"}
+  };
 });
 
-test("on domain", () => {
-  // TODO
+test("Test Loading With Darkmode ", () => {
+  var mockHTML = {style: {filter: undefined}};
+  mockDocument.querySelector.mockReturnValueOnce(mockHTML);
+  var c = content(mockStorage, mockDocument, mockWindow);
+  c.init();
+  expect(mockStorage.get).toHaveBeenCalledWith(["darkMode", "backgroundImg"], expect.any(Function));
+  mockStorage.get.mock.calls[0][1]({darkMode: true, backgroundImg: "https://example.com/pic.jpg"});
+  expect(mockDocument.body.style.backgroundImage).toEqual("url('https://example.com/pic.jpg')");
+  expect(mockDocument.body.style.backgroundRepeat).toEqual("no-repeat");
+  expect(mockDocument.body.style.backgroundSize).toEqual("contain");
+  expect(mockDocument.querySelector).toHaveBeenCalledWith("html");
+  expect(mockHTML.style.filter).toEqual("invert(1) hue-rotate(180deg)");
 });
-
-test("toggle", () => {
-  // TODO
+test("Test Loading Without Darkmode", () => {
+  var mockHTML = {style: {filter: undefined}};
+  mockDocument.querySelector.mockReturnValueOnce(mockHTML);
+  var c = content(mockStorage, mockDocument, mockWindow);
+  c.init();
+  expect(mockStorage.get).toHaveBeenCalledWith(["darkMode", "backgroundImg"], expect.any(Function));
+  mockStorage.get.mock.calls[0][1]({darkMode: false, backgroundImg: "https://example2.com/pic.jpg"});
+  expect(mockDocument.body.style.backgroundImage).toEqual("url('https://example2.com/pic.jpg')");
+  expect(mockDocument.body.style.backgroundRepeat).toEqual("no-repeat");
+  expect(mockDocument.body.style.backgroundSize).toEqual("contain");
+  expect(mockDocument.querySelector).toHaveBeenCalledWith("html");
+  expect(mockHTML.style.filter).toEqual("");
 });
--- js/content.js
+++ js/content.js
@@ -1,34 +1,34 @@
-const domain = window.location.origin;
-const url = window.location.href;
+function content(storage, doc, win) {
+  const domain = win.location.origin;
 
-isDomain();
-function isDomain() {
-  if (domain.includes("canvas") || domain.includes("instructure")) {
-    chrome.storage.sync.get(["darkMode", "backgroundImg"], function (result) {
-      setDarkMode(result.darkMode);
-      if (result.backgroundImg) {
-        document.body.style.backgroundImage = "url('" + result.backgroundImg + "')";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.backgroundSize = "contain";
-      }
-    });
+  function init() {
+    if (domain.includes("canvas") || domain.includes("instructure")) {
+      storage.get(["darkMode", "backgroundImg"], function (result) {
+        setDarkMode(result.darkMode);
+        if (result.backgroundImg) {
+          doc.body.style.backgroundImage = "url('" + result.backgroundImg + "')";
+          doc.body.style.backgroundRepeat = "no-repeat";
+          doc.body.style.backgroundSize = "contain";
+        }
+      });
+    }
+  }
+
+  function setDarkMode(enabled) {
+    doc.querySelector("html").style.filter = enabled
+      ? "invert(1) hue-rotate(180deg)"
+      : "";
+  }
+  return{
+    init: init,
+    setDarkMode: setDarkMode,
   }
 }
 
-function setDarkMode(enabled) {
-  document.querySelector("html").style.filter = enabled
-    ? "invert(1) hue-rotate(180deg)"
-    : "";
+// testing
+if (typeof exports !== "undefined") {
+  module.exports = content;
+} else {
+  content(chrome.storage.sync, document, window).init();
 }
 
-function toggle() {
-  var lightMode = document.getElementById("style1"),
-    darkMode = document.getElementById("style2");
-  if (lightMode.disabled === "disabled") {
-    lightMode.disabled = undefined;
-    darkMode.disabled = "disabled";
-  } else {
-    lightMode.disabled = "disabled";
-    darkMode.disabled = undefined;
-  }
-}
