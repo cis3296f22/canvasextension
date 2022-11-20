da819b006710d899bc777023f432241bc99a1b9e
tweiss9 <tylerhweiss@gmail.com>
2022-11-19 20:58:38 -0500

    more testing, fixed css code

--- js/popup.js
+++ js/popup.js
@@ -14,45 +14,58 @@ function popup(tabs, storage, scripting, doc) {
       .getElementById("backgroundButton")
       .addEventListener("click", onBackgroundClick);
 
-    doc.getElementById("sideMenuButton1").addEventListener("click", rmAccountClick);
-    doc.getElementById("sideMenuButton2").addEventListener("click", rmCalendarClick);
-    doc.getElementById("sideMenuButton3").addEventListener("click", rmCommonsClick);
-    doc.getElementById("sideMenuButton4").addEventListener("click", rmCoursesClick);
-    doc.getElementById("sideMenuButton5").addEventListener("click", rmGroupsClick);
-    doc.getElementById("sideMenuButton6").addEventListener("click", rmHelpClick);
-    doc.getElementById("sideMenuButton7").addEventListener("click", rmHistoryClick);
-    doc.getElementById("sideMenuButton8").addEventListener("click", rmInboxClick);
+    setupNavLink("sideMenuButton1", "global_nav_profile_link");
+    setupNavLink("sideMenuButton2", "global_nav_calendar_link");
+    setupNavLink("sideMenuButton3", "context_external_tool_9_menu_item");
+    setupNavLink("sideMenuButton4", "global_nav_courses_link");
+    setupNavLink("sideMenuButton5", "global_nav_groups_link");
+    setupNavLink("sideMenuButton6", "global_nav_help_link");
+    setupNavLink("sideMenuButton7", "global_nav_history_link");
+    setupNavLink("sideMenuButton8", "global_nav_conversations_link");
+
     doc.getElementById("colorPicker").addEventListener("click", colorChoice);
 
     doc
       .getElementById("darkModeToggle")
       .addEventListener("click", onDarkModeToggle, false);
 
-    storage.get(["darkMode", "backgroundImg", "rmHistory", "rmHelp", "rmCommons", "rmInbox", "rmCalendar", "rmGroups", "rmCourses", "rmAccount"],
-	 function (result) {
-      var backgroundTextBox = doc.getElementById("url_textbox");
-      var darkModeToggle = doc.getElementById("darkModeToggle");
-	  var history = doc.getElementById("sideMenuButton7");
-	  var help = doc.getElementById("sideMenuButton6");
-	  var commons = doc.getElementById("sideMenuButton3");
-	  var inbox = doc.getElementById("sideMenuButton8");
-	  var calendar = doc.getElementById("sideMenuButton2");
-	  var groups = doc.getElementById("sideMenuButton5");
-	  var courses = doc.getElementById("sideMenuButton4");
-	  var account = doc.getElementById("sideMenuButton1");
-	  
+    storage.get(
+      [
+        "darkMode",
+        "backgroundImg",
+        "rmHistory",
+        "rmHelp",
+        "rmCommons",
+        "rmInbox",
+        "rmCalendar",
+        "rmGroups",
+        "rmCourses",
+        "rmAccount",
+      ],
+      (result) => {
+        var backgroundTextBox = doc.getElementById("url_textbox");
+        var darkModeToggle = doc.getElementById("darkModeToggle");
+        var account = doc.getElementById("sideMenuButton1");
+        var calendar = doc.getElementById("sideMenuButton2");
+        var commons = doc.getElementById("sideMenuButton3");
+        var courses = doc.getElementById("sideMenuButton4");
+        var groups = doc.getElementById("sideMenuButton5");
+        var help = doc.getElementById("sideMenuButton6");
+        var history = doc.getElementById("sideMenuButton7");
+        var inbox = doc.getElementById("sideMenuButton8");
 
-      darkModeToggle.checked = result.darkMode;
-      backgroundTextBox.value = result.backgroundImg;
-	  history.checked = result.rmHistory;
-	  help.checked = result.rmHelp;
-	  commons.checked = result.rmCommons;
-	  inbox.checked = result.rmInbox;
-	  calendar.checked = result.rmCalendar;
-	  groups.checked = result.rmGroups;
-	  courses.checked = result.rmCourses;
-	  account.checked = result.rmAccount;
-    });
+        darkModeToggle.checked = result.darkMode;
+        backgroundTextBox.value = result.backgroundImg;
+        history.checked = result.rmHistory;
+        help.checked = result.rmHelp;
+        commons.checked = result.rmCommons;
+        inbox.checked = result.rmInbox;
+        calendar.checked = result.rmCalendar;
+        groups.checked = result.rmGroups;
+        courses.checked = result.rmCourses;
+        account.checked = result.rmAccount;
+      }
+    );
   }
 
   function onDarkModeToggle() {
@@ -60,16 +73,17 @@ function popup(tabs, storage, scripting, doc) {
     if (checked) {
       scripting.executeScript({
         target: { tabId: tabId, allFrames: true },
-        func: function(){
-          document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-        }
+        func: function () {
+          document.querySelector("html").style.filter =
+            "invert(1) hue-rotate(180deg)";
+        },
       });
     } else {
       scripting.executeScript({
         target: { tabId: tabId, allFrames: true },
-        func: function(){
+        func: function () {
           document.querySelector("html").style.filter = "";
-        }
+        },
       });
     }
     storage.set({ darkMode: checked });
@@ -89,194 +103,44 @@ function popup(tabs, storage, scripting, doc) {
     storage.set({ backgroundImg: url });
   }
 
-  function rmHistoryClick() {
-	var checked = doc.getElementById("sideMenuButton7").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_history_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmHistory: checked });
-  }
-    
-	
+  function setupNavLink(buttonId, elementId) {
+    var button = doc.getElementById(buttonId);
 
+    button.addEventListener("click", () => {
+      var checked = button.checked;
 
-  function rmHelpClick(){
-    var checked = doc.getElementById("sideMenuButton6").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_help_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmHelp: checked });
-  }
-
-  function rmCommonsClick(){
-    var checked = doc.getElementById("sideMenuButton3").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("context_external_tool_9_menu_item")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmCommons: checked });
-  }
-
-  function rmInboxClick(){
-    var checked = doc.getElementById("sideMenuButton8").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_conversations_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmInbox: checked });
-  }
-
-  function rmCalendarClick(){
-    var checked = doc.getElementById("sideMenuButton2").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_calendar_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmCalendar: checked });
-  }
-
-  function rmGroupsClick(){
-    var checked = doc.getElementById("sideMenuButton5").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_groups_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmGroups: checked });
-  }
-
-  function rmCoursesClick(){
-    var checked = doc.getElementById("sideMenuButton4").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_courses_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmCourses: checked });
-  }
-
-  function rmAccountClick(){
-    var checked = doc.getElementById("sideMenuButton1").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_profile_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmAccount: checked });
+      if (checked) {
+        scripting.executeScript({
+          target: { tabId: tabId, allFrames: true },
+          func: function (id) {
+            var meunList = document.getElementById(id);
+            meunList.style.display = "none";
+          },
+          args: [elementId]
+        });
+      } else {
+        scripting.executeScript({
+          target: { tabId: tabId, allFrames: true },
+          func: function (id) {
+            var meunList = document.getElementById(id);
+            meunList.style.display = null;        
+          },
+          args: [elementId]
+        });
+      }
+      storage.set({ rmHistory: checked });
+    });
   }
 
   function colorChoice() {
-     let color = document.getElementById('chooser').value;
-     document.body.style.backgroundColor = color;
+    let color = document.getElementById("chooser").value;
+    document.body.style.backgroundColor = color;
   }
 
   return {
     onDarkModeToggle: onDarkModeToggle,
     onBackgroundClick: onBackgroundClick,
-    init:init,
-    rmAccountClick: rmAccountClick,
-    rmCalendarClick: rmCalendarClick,
-    rmCommonsClick: rmCommonsClick,
-    rmCoursesClick: rmCoursesClick,
-    rmGroupsClick: rmGroupsClick,
-    rmHelpClick: rmHelpClick,
-    rmHistoryClick: rmHistoryClick,
-    rmInboxClick: rmInboxClick,
+    init: init,
   };
 }
 
--- popup.test.js
+++ popup.test.js
@@ -30,19 +30,106 @@ beforeEach(() => {
 
 test("popup initializes", () => {
   var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  expect(mockDocument.addEventListener).toHaveBeenCalledWith("DOMContentLoaded", expect.any(Function), false);
-  var mockEventListener = jest.fn();
-  mockDocument.getElementById.mockReturnValue({addEventListener: mockEventListener});
+  expect(mockDocument.addEventListener).toHaveBeenCalledWith(
+    "DOMContentLoaded",
+    expect.any(Function),
+    false
+  );
+
+  var mockElements = [];
+  mockDocument.getElementById.mockImplementation((id) => {
+    var element = {
+      addEventListener: jest.fn(),
+      id: id,
+    };
+
+    mockElements.push(element);
+    return element;
+  });
+
+  var findMockElem = (id) => mockElements.find((e) => e.id === id);
+
   p.init();
-  expect(mockDocument.getElementById).toHaveBeenCalledWith("backgroundButton");
-  expect(mockEventListener).toHaveBeenCalledWith("click", expect.any(Function));
-  expect(mockStorage.get).toHaveBeenCalledWith(["darkMode", "backgroundImg"], expect.any(Function));
-  var mockElements = {checked: undefined, value: undefined}
-  mockDocument.getElementById.mockReturnValue(mockElements);
-  mockStorage.get.mock.calls[0][1]({darkMode: true, backgroundImg: "https://example.com/pic.jpg"});
-  expect(mockElements.checked).toEqual(true);
-  expect(mockElements.value).toEqual("https://example.com/pic.jpg");
-  
+
+  var testClickWasSetup = (toggleId, stop) => {
+    var elem = findMockElem(toggleId);
+
+    expect(elem).toBeTruthy();
+    expect(elem.addEventListener).toHaveBeenCalledTimes(1);
+
+    if (stop) {
+      expect(elem.addEventListener).toHaveBeenCalledWith(
+        "click",
+        expect.any(Function),
+        false
+      );
+    } else {
+      expect(elem.addEventListener).toHaveBeenCalledWith(
+        "click",
+        expect.any(Function)
+      );
+    }
+  };
+
+  testClickWasSetup("backgroundButton");
+  testClickWasSetup("colorPicker");
+  testClickWasSetup("darkModeToggle", true);
+
+  testClickWasSetup("sideMenuButton1");
+  testClickWasSetup("sideMenuButton2");
+  testClickWasSetup("sideMenuButton3");
+  testClickWasSetup("sideMenuButton4");
+  testClickWasSetup("sideMenuButton5");
+  testClickWasSetup("sideMenuButton6");
+  testClickWasSetup("sideMenuButton7");
+  testClickWasSetup("sideMenuButton8");
+
+  expect(mockStorage.get).toHaveBeenCalledWith(
+    [
+      "darkMode",
+      "backgroundImg",
+      "rmHistory",
+      "rmHelp",
+      "rmCommons",
+      "rmInbox",
+      "rmCalendar",
+      "rmGroups",
+      "rmCourses",
+      "rmAccount",
+    ],
+    expect.any(Function)
+  );
+
+  mockElements = [];
+
+  mockStorage.get.mock.calls[0][1]({
+    backgroundImg: "https://example.com/pic.jpg",
+    darkMode: true,
+    rmHistory: true,
+    rmHelp: true,
+    rmCommons: true,
+    rmInbox: true,
+    rmCalendar: false,
+    rmGroups: true,
+    rmCourses: true,
+    rmAccount: false,
+  });
+
+  expect(mockElements.length).toEqual(10);
+
+  expect(findMockElem("sideMenuButton1").checked).toBe(false);
+  expect(findMockElem("sideMenuButton2").checked).toBe(false);
+  expect(findMockElem("sideMenuButton3").checked).toBe(true);
+  expect(findMockElem("sideMenuButton4").checked).toBe(true);
+  expect(findMockElem("sideMenuButton5").checked).toBe(true);
+  expect(findMockElem("sideMenuButton6").checked).toBe(true);
+  expect(findMockElem("sideMenuButton7").checked).toBe(true);
+  expect(findMockElem("sideMenuButton8").checked).toBe(true);
+
+  expect(findMockElem("url_textbox").value).toEqual(
+    "https://example.com/pic.jpg"
+  );
+  expect(findMockElem("darkModeToggle").checked).toEqual(true);
 });
 
 test("toggle dark mode", () => {
@@ -86,84 +173,44 @@ test("set background", () => {
     backgroundImg: "https://example.com/pic.jpg",
   });
 });
-test("rmHistoryClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmHistoryClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
+
+test("test remove click", () => {
+  var mockElements = [];
+  mockDocument.getElementById.mockImplementation((id) => {
+    var element = {
+      addEventListener: jest.fn(),
+      id: id,
+    };
+
+    if (id.startsWith("sideMenuButton")) {
+      mockElements.push(element);
+    }
+
+    return element;
   });
-});
-test("rmHelpClick", () => {
+
   var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmHelpClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
-});
-test("rmCommonsClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmCommonsClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
-});
-test("rmInboxClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmInboxClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
-});
-test("rmCalendarClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmCalendarClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
-});
-test("rmGroupsClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmInboxClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
-});
-test("rmGroupsClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmGroupsClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
-});
-test("rmCoursesClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmCoursesClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
-});
-test("rmAccountClick", () => {
-  var p = popup(mockTabs, mockStorage, mockScripting, mockDocument);
-  p.rmAccountClick();
-  expect(mockScripting.executeScript.mock.calls.length).toEqual(1);
-  expect(mockScripting.executeScript).toHaveBeenCalledWith({
-    target: expectedTarget,
-    func: expect.any(Function),
-  });
+  p.init();
+
+  // call all click functions
+  mockElements.forEach((e) => e.addEventListener.mock.calls[0][1]());
+
+  console.log(mockStorage);
+
+  var testIt = (id) => {
+    expect(mockScripting.executeScript).toHaveBeenCalledWith({
+      target: expectedTarget,
+      func: expect.any(Function),
+      args: [id],
+    });
+  };
+
+  testIt("global_nav_profile_link");
+  testIt("global_nav_calendar_link");
+  testIt("context_external_tool_9_menu_item");
+  testIt("global_nav_courses_link");
+  testIt("global_nav_groups_link");
+  testIt("global_nav_help_link");
+  testIt("global_nav_history_link");
+  testIt("global_nav_conversations_link");
 });
