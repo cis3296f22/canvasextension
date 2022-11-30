a4c73bc3ab06cf8618ff92990075daa2e091c49e
Cindy Tang <tuk28810@temple.edu>
2022-11-30 00:47:43 -0500

    Catching up from main branch

--- js/content.js
+++ js/content.js
@@ -3,23 +3,38 @@ function content(storage, doc, win) {
 
   function init() {
     if (domain.includes("canvas") || domain.includes("instructure")) {
-      storage.get(["darkMode", "backgroundImg", "rmHistory", "rmHelp", "rmCommons", "rmInbox", "rmCalendar", "rmGroups", "rmCourses", "rmAccount"], function (result) {
-        setDarkMode(result.darkMode);
-		setHistorySideBar(result.rmHistory);
-		setHelpSideBar(result.rmHelp);
-		setCommonsSideBar(result.rmCommons);
-		setInboxSideBar(result.rmInbox);
-		setCalendarSideBar(result.rmCalendar);
-		setGroupsSideBar(result.rmGroups);
-		setCoursesSideBar(result.rmCourses);
-		setAccountSideBar(result.rmAccount);
+      storage.get(
+        [
+          "darkMode",
+          "backgroundImg",
+          "rmHistory",
+          "rmHelp",
+          "rmCommons",
+          "rmInbox",
+          "rmCalendar",
+          "rmGroups",
+          "rmCourses",
+          "rmAccount",
+        ],
+        function (result) {
+          setDarkMode(result.darkMode);
+          setHistorySideBar(result.rmHistory);
+          setHelpSideBar(result.rmHelp);
+          setCommonsSideBar(result.rmCommons);
+          setInboxSideBar(result.rmInbox);
+          setCalendarSideBar(result.rmCalendar);
+          setGroupsSideBar(result.rmGroups);
+          setCoursesSideBar(result.rmCourses);
+          setAccountSideBar(result.rmAccount);
 
-        if (result.backgroundImg) {
-          doc.body.style.backgroundImage = "url('" + result.backgroundImg + "')";
-          doc.body.style.backgroundRepeat = "no-repeat";
-          doc.body.style.backgroundSize = "contain";
+          if (result.backgroundImg) {
+            doc.body.style.backgroundImage =
+              "url('" + result.backgroundImg + "')";
+            doc.body.style.backgroundRepeat = "no-repeat";
+            doc.body.style.backgroundSize = "contain";
+          }
         }
-      });
+      );
     }
   }
 
@@ -28,66 +43,76 @@ function content(storage, doc, win) {
       ? "invert(1) hue-rotate(180deg)"
       : "";
   }
-  return{
+
+  function setHistorySideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById("global_nav_history_link");
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+
+  function setHelpSideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById("global_nav_help_link");
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+
+  function setCommonsSideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById(
+        "context_external_tool_9_menu_item"
+      );
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+
+  function setInboxSideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById("global_nav_conversations_link");
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+
+  function setCalendarSideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById("global_nav_calendar_link");
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+
+  function setGroupsSideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById("global_nav_groups_link");
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+
+  function setCoursesSideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById("global_nav_courses_link");
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+
+  function setAccountSideBar(disabled) {
+    if (disabled) {
+      var menuList = document.getElementById("global_nav_profile_link");
+      if (menuList) menuList.style.display = "none";
+    }
+  }
+  return {
     init: init,
     setDarkMode: setDarkMode,
-  }
-
-  function setHistorySideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("global_nav_history_link")
-		meunList.remove();
-	}
-  }
-
-  function setHelpSideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("global_nav_help_link")
-		meunList.remove();
-	}
-  }
-
-  function setCommonsSideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("context_external_tool_9_menu_item")
-		meunList.remove();
-	}
-  }
-
-  function setInboxSideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("global_nav_conversations_link")
-		meunList.remove();
-	}
-  }
-
-  function setCalendarSideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("global_nav_calendar_link")
-		meunList.remove();
-	}
-  }
-
-  function setGroupsSideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("global_nav_groups_link")
-		meunList.remove();
-	}
-  }
-
-  function setCoursesSideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("global_nav_courses_link")
-		meunList.remove();
-	}
-  }
-
-  function setAccountSideBar(disabled){
-	if(disabled){
-		var meunList = document.getElementById("global_nav_profile_link")
-		meunList.remove();
-	}
-  }
+    setHistorySideBar: setHistorySideBar,
+    setHelpSideBar: setHelpSideBar,
+    setCommonsSideBar: setCommonsSideBar,
+    setInboxSideBar: setInboxSideBar,
+    setCalendarSideBar: setCalendarSideBar,
+    setGroupsSideBar: setGroupsSideBar,
+    setCoursesSideBar: setCoursesSideBar,
+    setAccountSideBar: setAccountSideBar,
+  };
 }
 
 // testing
@@ -96,4 +121,3 @@ if (typeof exports !== "undefined") {
 } else {
   content(chrome.storage.sync, document, window).init();
 }
-
--- js/popup.js
+++ js/popup.js
@@ -14,45 +14,62 @@ function popup(tabs, storage, scripting, doc) {
       .getElementById("backgroundButton")
       .addEventListener("click", onBackgroundClick);
 
-    doc.getElementById("sideMenuButton1").addEventListener("click", rmAccountClick);
-    doc.getElementById("sideMenuButton2").addEventListener("click", rmCalendarClick);
-    doc.getElementById("sideMenuButton3").addEventListener("click", rmCommonsClick);
-    doc.getElementById("sideMenuButton4").addEventListener("click", rmCoursesClick);
-    doc.getElementById("sideMenuButton5").addEventListener("click", rmGroupsClick);
-    doc.getElementById("sideMenuButton6").addEventListener("click", rmHelpClick);
-    doc.getElementById("sideMenuButton7").addEventListener("click", rmHistoryClick);
-    doc.getElementById("sideMenuButton8").addEventListener("click", rmInboxClick);
+    setupNavLink("sideMenuButton1", "global_nav_profile_link", "rmAccount");
+    setupNavLink("sideMenuButton2", "global_nav_calendar_link", "rmCalendar");
+    setupNavLink("sideMenuButton3", "context_external_tool_9_menu_item", "rmCommons");
+    setupNavLink("sideMenuButton4", "global_nav_courses_link", "rmCourses");
+    setupNavLink("sideMenuButton5", "global_nav_groups_link", "rmGroups");
+    setupNavLink("sideMenuButton6", "global_nav_help_link", "rmHelp");
+    setupNavLink("sideMenuButton7", "global_nav_history_link", "rmHistory");
+    setupNavLink("sideMenuButton8", "global_nav_conversations_link", "rmInbox");
+
     doc.getElementById("colorPicker").addEventListener("click", colorChoice);
 
     doc
       .getElementById("darkModeToggle")
       .addEventListener("click", onDarkModeToggle, false);
 
-    storage.get(["darkMode", "backgroundImg", "rmHistory", "rmHelp", "rmCommons", "rmInbox", "rmCalendar", "rmGroups", "rmCourses", "rmAccount"],
-	 function (result) {
-      var backgroundTextBox = doc.getElementById("url_textbox");
-      var darkModeToggle = doc.getElementById("darkModeToggle");
-	  var history = doc.getElementById("sideMenuButton7");
-	  var help = doc.getElementById("sideMenuButton6");
-	  var commons = doc.getElementById("sideMenuButton3");
-	  var inbox = doc.getElementById("sideMenuButton8");
-	  var calendar = doc.getElementById("sideMenuButton2");
-	  var groups = doc.getElementById("sideMenuButton5");
-	  var courses = doc.getElementById("sideMenuButton4");
-	  var account = doc.getElementById("sideMenuButton1");
-	  
+    storage.get(
+      [
+        "darkMode",
+        "backgroundImg",
+        "rmHistory",
+        "rmHelp",
+        "rmCommons",
+        "rmInbox",
+        "rmCalendar",
+        "rmGroups",
+        "rmCourses",
+        "rmAccount",
+      ],
+      (result) => {
+        var backgroundTextBox = doc.getElementById("url_textbox");
+        var darkModeToggle = doc.getElementById("darkModeToggle");
 
-      darkModeToggle.checked = result.darkMode;
-      backgroundTextBox.value = result.backgroundImg;
-	  history.checked = result.rmHistory;
-	  help.checked = result.rmHelp;
-	  commons.checked = result.rmCommons;
-	  inbox.checked = result.rmInbox;
-	  calendar.checked = result.rmCalendar;
-	  groups.checked = result.rmGroups;
-	  courses.checked = result.rmCourses;
-	  account.checked = result.rmAccount;
-    });
+        // TODO: move to setUpNavLink
+        var account = doc.getElementById("sideMenuButton1");
+        var calendar = doc.getElementById("sideMenuButton2");
+        var commons = doc.getElementById("sideMenuButton3");
+        var courses = doc.getElementById("sideMenuButton4");
+        var groups = doc.getElementById("sideMenuButton5");
+        var help = doc.getElementById("sideMenuButton6");
+        var history = doc.getElementById("sideMenuButton7");
+        var inbox = doc.getElementById("sideMenuButton8");
+
+        darkModeToggle.checked = result.darkMode;
+        backgroundTextBox.value = result.backgroundImg;
+
+        //TODO: move to setupNavLink
+        history.checked = !result.rmHistory;
+        help.checked = !result.rmHelp;
+        commons.checked = !result.rmCommons;
+        inbox.checked = !result.rmInbox;
+        calendar.checked = !result.rmCalendar;
+        groups.checked = !result.rmGroups;
+        courses.checked = !result.rmCourses;
+        account.checked = !result.rmAccount;
+      }
+    );
   }
 
   function onDarkModeToggle() {
@@ -60,16 +77,17 @@ function popup(tabs, storage, scripting, doc) {
     if (checked) {
       scripting.executeScript({
         target: { tabId: tabId, allFrames: true },
-        func: function(){
-          document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-        }
+        func: function () {
+          document.querySelector("html").style.filter =
+            "invert(1) hue-rotate(180deg)";
+        },
       });
     } else {
       scripting.executeScript({
         target: { tabId: tabId, allFrames: true },
-        func: function(){
+        func: function () {
           document.querySelector("html").style.filter = "";
-        }
+        },
       });
     }
     storage.set({ darkMode: checked });
@@ -89,194 +107,33 @@ function popup(tabs, storage, scripting, doc) {
     storage.set({ backgroundImg: url });
   }
 
-  function rmHistoryClick() {
-	var checked = doc.getElementById("sideMenuButton7").checked;
-    if (checked) {
+  function setupNavLink(buttonId, elementId, storageId) {
+    var button = doc.getElementById(buttonId);
+    button.addEventListener("click", () => {
+    var checked = button.checked;
       scripting.executeScript({
         target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_history_link")
-			meunList.remove();
-        }
+        func: function (id, show) {
+          var menulist = document.getElementById(id);
+          menulist.style.display = show ? null : "none";
+        },
+        args: [elementId, checked]
       });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmHistory: checked });
-  }
-    
-	
-
-
-  function rmHelpClick(){
-    var checked = doc.getElementById("sideMenuButton6").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_help_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmHelp: checked });
-  }
-
-  function rmCommonsClick(){
-    var checked = doc.getElementById("sideMenuButton3").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("context_external_tool_9_menu_item")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmCommons: checked });
-  }
-
-  function rmInboxClick(){
-    var checked = doc.getElementById("sideMenuButton8").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_conversations_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmInbox: checked });
-  }
-
-  function rmCalendarClick(){
-    var checked = doc.getElementById("sideMenuButton2").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_calendar_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmCalendar: checked });
-  }
-
-  function rmGroupsClick(){
-    var checked = doc.getElementById("sideMenuButton5").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_groups_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmGroups: checked });
-  }
-
-  function rmCoursesClick(){
-    var checked = doc.getElementById("sideMenuButton4").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_courses_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmCourses: checked });
-  }
-
-  function rmAccountClick(){
-    var checked = doc.getElementById("sideMenuButton1").checked;
-    if (checked) {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-			var meunList = document.getElementById("global_nav_profile_link")
-			meunList.remove();
-        }
-      });
-    } else {
-      scripting.executeScript({
-        target: { tabId: tabId, allFrames: true },
-        func: function(){
-          //TODO: add option back
-        }
-      });
-    }
-    storage.set({ rmAccount: checked });
+      storage.set({ [storageId] : !checked });
+    });
   }
 
   function colorChoice() {
-     let color = document.getElementById('chooser').value;
-     document.body.style.backgroundColor = color;
+    let color = doc.getElementById("chooser").value;
+    doc.body.style.backgroundColor = color;
   }
 
   return {
     onDarkModeToggle: onDarkModeToggle,
     onBackgroundClick: onBackgroundClick,
-    init:init,
-    rmAccountClick: rmAccountClick,
-    rmCalendarClick: rmCalendarClick,
-    rmCommonsClick: rmCommonsClick,
-    rmCoursesClick: rmCoursesClick,
-    rmGroupsClick: rmGroupsClick,
-    rmHelpClick: rmHelpClick,
-    rmHistoryClick: rmHistoryClick,
-    rmInboxClick: rmInboxClick,
+    init: init,
+    setupNavLink: setupNavLink,
+    colorChoice: colorChoice,
   };
 }
 
